a83b73e3a81ca3a62a7307ddfb72237b62111944
kojihyung <kokoleerh@gmail.com>
2020-08-07 16:31:33 +0900

    bugfix

--- relayA/quickstart/serializers.py
+++ relayA/quickstart/serializers.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import User, Group
 from .models import Diary
 from rest_framework import serializers
 
-
 class DiarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Diary
-        fields = ('title','content','tags')
+        fields = ('title','content','tags',)
